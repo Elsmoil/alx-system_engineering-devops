30dabbad2df5bf07d3b2765a0f272a14b2fc756e
Elsmoil <alsmoalsuliman@gmail.com>
2024-09-02 17:46:51 -0700

    more

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-#Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing)
+#Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet.
 exec { 'fix-wordpress':
   command => 'chmod -R 755 /var/www/html/wp-content/themes/twentyseventeen',
   path    => '/bin',
