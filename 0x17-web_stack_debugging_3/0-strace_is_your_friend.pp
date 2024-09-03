bded551be1b1c1ca6d872b504f1233fd8b2281c3
Elsmoil <alsmoalsuliman@gmail.com>
2024-09-02 20:39:56 -0700

    final

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet.
-exec { 'fix-wordpress':
+exec { 'fix-wordpress-server-error':
   command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/bin/:/bin/',
 }
