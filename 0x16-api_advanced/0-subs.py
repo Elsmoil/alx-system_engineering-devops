2128074cf01165bedfe0b4507799f51513e41154
Elsmoil <alsmoalsuliman@gmail.com>
2024-09-03 17:05:20 -0700

    Write a function that queries the Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+0-subs module
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+        """
+            Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+                Args:
+                        subreddit (str): The name of the subreddit.
+
+                            Returns:
+                                        int: The number of subscribers for the given subreddit. If the subreddit is invalid, returns 0.
+                                            """
+                                                url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+                                                    headers = {"User-Agent": "My Reddit API Client"}
+                                                        response = requests.get(url, headers=headers, allow_redirects=False)
+
+                                                            if response.status_code == 200:
+                                                                        data = response.json()
+                                                                                return data["data"]["subscribers"]
+                                                                                    else:
+                                return 0
