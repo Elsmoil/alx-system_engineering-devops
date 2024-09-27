f7ad82a44e94c62c96e0c5c2508b818950a2d12c
Elsmoil <alsmoalsuliman@gmail.com>
2024-09-27 14:09:15 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,21 +6,26 @@ This module contains a function to query the Reddit API and return the number of
 subscribers for a given subreddit. If the subreddit is invalid, it returns 0.
 """
 
-import requests  # Importing the requests module in alphabetical order
+import requests
+
 
 def number_of_subscribers(subreddit):
     """
-        Queries the Reddit API and returns the number of subscribers for a given subreddit.
-            Args:
-                subreddit (str): The name of the subreddit.
-                        Returns:
-                            int: The number of subscribers for the given subreddit. If the subreddit is invalid, returns 0.
-                                    """
-                                        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-                                            headers = {"User-Agent": "My Reddit API Client"}
-                                                response = requests.get(url, headers=headers, allow_redirects=False)
-                                                    if response.status_code == 200:
-                                                            data = response.json()
-                                                                    return data["data"]["subscribers"]
-                                                                        else:
-                                                                                return 0
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+        
+    Returns:
+        int: The number of subscribers for the given subreddit. If the subreddit
+        is invalid, returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My Reddit API Client"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
