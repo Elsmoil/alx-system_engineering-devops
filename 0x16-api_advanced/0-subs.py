3fd850f1ef472feae213412142a4ff4931c07296
Elsmoil <alsmoalsuliman@gmail.com>
2024-09-27 14:38:19 -0700

    fix PEP-8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,18 +12,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-
     """
     Queries the Reddit API and returns the number of subscribers
     for a given subreddit.
-
     Args:
         subreddit (str): The name of the subreddit.
 
     Returns:
         int: The number of subscribers for the given subreddit.
         If the subreddit is invalid, returns 0.
-"""
+    """
 
 
 url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -32,6 +30,6 @@ response = requests.get(url, headers=headers, allow_redirects=False)
 
 if response.status_code == 200:
     data = response.json()
-    return data["data"]["subscribers"]
+    return data.get("data", {}).get("subscribers", 0)
 else:
     return 0
